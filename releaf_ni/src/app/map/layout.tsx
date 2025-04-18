253330aff64d8d5c98435b3b7d88abf16f964020
emulholland2002 <ethan_mul@outlook.com>
2025-04-18 23:51:17 +0100

    adding bg colour

--- releaf_ni/src/app/map/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type React from "react"
-export default function MapLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <main className="min-h-screen bg-background">{children}</main>
-}
